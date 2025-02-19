f70bb800513d8c477a3d6b952820ad08007c6c22
agustinmasso <agustinmasso@hotmail.com>
2025-02-19 20:04:10 -0300

    arreglado la lista de productos, y las rutas en app.jsx

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { FaShoppingCart } from 'react-icons/fa';
-import './CartWidget.css';
+import Badge from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useContext } from "react";
+import { CartContext } from "../../../context/CartContext";
 
-const CartWidget = ({ count }) => {
+
+
+const CartWidget = () => {
+  const { getTotalQuantity } = useContext(CartContext);
+  const total = getTotalQuantity();
+
   return (
-    <div className="cart-widget">
-      <FaShoppingCart />
-      {count > 0 && <span className="cart-count">{count}</span>}
+    <div>
+      <Badge badgeContent={total} color="secondary" showZero={true}>
+        <ShoppingCartIcon />
+      </Badge>
     </div>
   );
 };
