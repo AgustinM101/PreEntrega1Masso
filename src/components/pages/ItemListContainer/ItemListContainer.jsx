e8a6069996722af755ea89b7b07e7500127e032d
agustinmasso <agustinmasso@hotmail.com>
2025-02-25 20:47:27 -0300

    Añadiendo estilos al carrito y arreglando logica del carrito y cart context

--- src/components/pages/ItemListContainer/ItemListContainer.jsx
+++ src/components/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import './ItemListContainer.css';
-import ProductCard from '../../common/productCard/ProductCard';
-import { products } from '../../../products';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./ItemListContainer.css";
+import ProductCard from "../../common/productCard/ProductCard";
+import { products } from "../../../products";
+import { useParams } from "react-router-dom";
 
 const ItemListContainer = ({ greeting }) => {
   const { name } = useParams();
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    let arrayFiltrado = products.filter((elemento) => elemento.category === name);
+    let arrayFiltrado = products.filter(
+      (elemento) => elemento.category === name
+    );
 
     const getProducts = new Promise((resolve, reject) => {
       let permiso = true;
       if (permiso) {
         resolve(name ? arrayFiltrado : products);
       } else {
-        reject({ status: 400, message: 'algo salió mal' });
+        reject({ status: 400, message: "algo salió mal" });
       }
     });
 
@@ -33,10 +35,44 @@ const ItemListContainer = ({ greeting }) => {
     <div className="item-list-container">
       <h2>{greeting}</h2>
       <div className="categories">
-        <button className="btn btn-secondary" onClick={() => setItems(products.filter((product) => product.category === 'vuelo'))}>Simuladores de Vuelo</button>
-        <button className="btn btn-secondary" onClick={() => setItems(products.filter((product) => product.category === 'carreras'))}>Simuladores de Carreras</button>
-        <button className="btn btn-secondary" onClick={() => setItems(products.filter((product) => product.category === 'espacio'))}>Simuladores de Espacio</button>
-        <button className="btn btn-secondary" onClick={() => setItems(products.filter((product) => product.category === 'shooters'))}>Simuladores de Shooters</button>
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setItems(products.filter((product) => product.category === "vuelo"))
+          }
+        >
+          Simuladores de Vuelo
+        </button>
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setItems(
+              products.filter((product) => product.category === "carreras")
+            )
+          }
+        >
+          Simuladores de Carreras
+        </button>
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setItems(
+              products.filter((product) => product.category === "espacio")
+            )
+          }
+        >
+          Simuladores de Espacio
+        </button>
+        <button
+          className="btn btn-secondary"
+          onClick={() =>
+            setItems(
+              products.filter((product) => product.category === "shooters")
+            )
+          }
+        >
+          Simuladores de Shooters
+        </button>
       </div>
       <div className="item-list">
         {items.map((item) => (
