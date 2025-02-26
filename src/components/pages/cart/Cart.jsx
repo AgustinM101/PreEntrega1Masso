1d2e45b754b998e45f02fbc103bd017a0df74c4e
agustinmasso <agustinmasso@hotmail.com>
2025-02-25 21:21:44 -0300

    corregido el boton de agregar al carrito y modificando estilos, ademas agregado skeleton

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,32 +1,96 @@
-import React, { useContext } from "react";
+import { Button, IconButton } from "@mui/material";
+import { useContext } from "react";
+import { Link } from "react-router-dom"; // Cambiado de "react-router" a "react-router-dom"
 import { CartContext } from "../../../context/CartContext";
+import Swal from "sweetalert2";
+import CancelIcon from "@mui/icons-material/Cancel";
 import "./Cart.css";
 
 const Cart = () => {
-  const { cart, getTotalAmount, removeById, resetCart } =
+  const { resetCart, cart, removeById, getTotalAmount } =
     useContext(CartContext);
+
+  let total = getTotalAmount();
+
+  const resetCartWithAlert = () => {
+    Swal.fire({
+      title: "Seguro quieres vaciar el carrito?",
+      showDenyButton: true,
+      showCancelButton: false,
+      confirmButtonText: "Si, vaciar",
+      denyButtonText: `No, dejar como estaba`,
+    }).then((result) => {
+      console.log(result);
+      if (result.isConfirmed) {
+        resetCart();
+        Swal.fire({
+          title: "Carrito vaciado con exito",
+          showDenyButton: false,
+          showCancelButton: false,
+          confirmButtonText: "Ok",
+          icon: "success",
+        });
+      } else if (result.isDenied) {
+        Swal.fire({
+          title: "El carrito queda como estaba",
+          showDenyButton: false,
+          showCancelButton: false,
+          confirmButtonText: "Ok",
+          icon: "info",
+        });
+      }
+    });
+  };
+
+  const handleRemoveItem = (id) => {
+    removeById(id);
+    Swal.fire({
+      title: "Producto eliminado",
+      icon: "warning",
+      confirmButtonText: "Ok",
+    });
+  };
 
   return (
     <div className="cart-container">
       <h2>Carrito de Compras</h2>
       {cart.length === 0 ? (
-        <p>El carrito esta vacio</p>
+        <p>El carrito está vacío</p>
       ) : (
         <div>
-          {cart.map((item) => (
-            <div key={item.id} className="cart-item">
-              <img src={item.image} alt={item.name} />
+          {cart.map((elemento) => (
+            <div key={elemento.id} className="cart-item">
+              <img src={elemento.image} alt={elemento.name} />
               <div>
-                <h3>{item.name}</h3>
-                <p>{item.description}</p>
-                <p>${item.price}</p>
-                <button onClick={() => removeById(item.id)}>Eliminar</button>
+                <h3>{elemento.name}</h3>
+                <p>{elemento.description}</p>
+                <p>${elemento.price}</p>
+                <IconButton
+                  color="secondary"
+                  onClick={() => handleRemoveItem(elemento.id)}
+                >
+                  <CancelIcon />
+                </IconButton>
               </div>
             </div>
           ))}
           <div className="cart-total">
-            <h3>Total: ${getTotalAmount()}</h3>
-            <button onClick={resetCart}>Vaciar Carrito</button>
+            <h3>Total: ${total}</h3>
+            <Button
+              variant="outlined"
+              color="secondary"
+              onClick={resetCartWithAlert}
+            >
+              Vaciar carrito
+            </Button>
+            <Button variant="contained" color="primary">
+              <Link
+                to="/checkout"
+                style={{ color: "inherit", textDecoration: "none" }}
+              >
+                Finalizar compra
+              </Link>
+            </Button>
           </div>
         </div>
       )}
