0817c836121f78e13d5d991a8e8110bafd580b12
agustinmasso <agustinmasso@hotmail.com>
2025-03-11 06:33:22 -0300

    recuperando cambios checkout,cart,formulario e itemdetail

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './NavBar.css';
-import { Link } from 'react-router-dom';
-import Badge from '@mui/material/Badge';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useContext } from 'react';
-import { CartContext } from '../../context/CartContext';
+import React from "react";
+import "./NavBar.css";
+import { Link } from "react-router-dom";
+import Badge from "@mui/material/Badge";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
 
 const NavBar = () => {
   const { getTotalQuantity } = useContext(CartContext);
@@ -12,17 +12,26 @@ const NavBar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">Tienda de Simuladores</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <Link className="navbar-brand" to="/">
+        Tienda de Simuladores
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/">Inicio</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/categories">Categorías</Link>
+            <Link className="nav-link" to="/">
+              Inicio
+            </Link>
           </li>
         </ul>
         <Link className="nav-link" to="/cart">
