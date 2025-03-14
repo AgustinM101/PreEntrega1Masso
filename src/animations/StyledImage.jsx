21da41c67bab5707bc6c436cf1a2d38f0f50bd74
agustinmasso <agustinmasso@hotmail.com>
2025-03-14 16:29:39 -0300

    gradiante de colores modificado en StyledImage

--- src/animations/StyledImage.jsx
+++ src/animations/StyledImage.jsx
@@ -2,20 +2,20 @@ import styled, { keyframes } from "styled-components";
 
 const borderAnimation = keyframes`
   0% {
-    border-color: cyan;
+    border-color: #5d4037; /* Color inicial del borde */
   }
   50% {
-    border-color: magenta;
+    border-color:rgb(189, 133, 106); /* Color intermedio del borde */
   }
   100% {
-    border-color: cyan;
+    border-color: #d7a86e; /* Color final del borde */
   }
 `;
 
 const StyledImage = styled.img`
-  width: 50%; /* Ajustar el ancho de la imagen */
+  width: 30%; /* Ajustar el ancho de la imagen */
   height: auto;
-  border: 5px solid cyan;
+  border: 3px solid #d1d1d1;
   animation: ${borderAnimation} 5s infinite;
   border-radius: 10px;
   margin: 0 auto; /* Centrar la imagen horizontalmente */
