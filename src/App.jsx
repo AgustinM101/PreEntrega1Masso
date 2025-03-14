fd74d5e6884ecd872c49caaaaa4be233cc9007ed
agustinmasso <agustinmasso@hotmail.com>
2025-03-14 17:49:36 -0300

    agregado error404 con fuzzytext en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,14 @@ import ItemListContainer from "./components/pages/ItemListContainer/ItemListCont
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CartContextProvider from "./context/CartContext";
 import DarkModeContextProvider from "./context/DarkModeContext";
+import FuzzyText from "./components/common/fuzzyText/FuzzyText";
+import "./styles/styles.css";
 
 function App() {
+  // Definir las variables hoverIntensity y enableHover
+  const hoverIntensity = 0.5;
+  const enableHover = true;
+
   return (
     <Router>
       <CartContextProvider>
@@ -22,13 +28,27 @@ function App() {
           <Route path="/itemDetail/:id" component={ItemDetail} />
           <Route path="/checkout" component={Checkout} />
 
-          <Route path="*" component={() => <h2>404 not found</h2>} />
+          <Route
+            path="*"
+            component={() => (
+              <div class="centered-container">
+                <FuzzyText
+                  baseIntensity={0.2}
+                  hoverIntensity={hoverIntensity}
+                  enableHover={enableHover}
+                >
+                  404 ERROR - Página no encontrada
+                </FuzzyText>
+              </div>
+            )}
+          />
         </Switch>
         {/* </DarkModeContextProvider> */}
       </CartContextProvider>
     </Router>
   );
 }
+
 // se utiliza Switch ya que tuve problemas por la version de react-router-dom, que es anteterior a la version 6
 
 export default App;
