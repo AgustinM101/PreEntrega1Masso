8221db5f7f160254e0a81cea881d3a9df46e3ce7
agustinmasso <agustinmasso@hotmail.com>
2025-03-18 20:42:01 -0300

    cambios en rotatingtext y navbar

--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,19 +1,50 @@
 import React from "react";
 import "./NavBar.css";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Badge from "@mui/material/Badge";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
+import RotatingText from "../common/RotatingText/RotatingText";
 
 const NavBar = () => {
   const { getTotalQuantity } = useContext(CartContext);
   const cartItemCount = getTotalQuantity();
+  const history = useHistory();
+
+  const handleHomeClick = () => {
+    history.push("/");
+    window.location.reload(); // Recargar la página para restablecer el estado
+  };
+
+  <RotatingText
+    texts={["React", "Bits", "Is", "Cool!"]}
+    mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+    staggerFrom={"last"}
+    initial={{ y: "100%" }}
+    animate={{ y: 0 }}
+    exit={{ y: "-120%" }}
+    staggerDuration={0.025}
+    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+    transition={{ type: "spring", damping: 30, stiffness: 400 }}
+    rotationInterval={2000}
+  />;
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/">
-        Tienda de Simuladores
+      <Link className="navbar-brand" to="/" onClick={handleHomeClick}>
+        <RotatingText
+          texts={[" Racing", "Fps", "Flight", "Space"]}
+          mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+          staggerFrom={"last"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.03}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={3000}
+        />
       </Link>
       <button
         className="navbar-toggler"
@@ -29,7 +60,7 @@ const NavBar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link className="nav-link" to="/">
+            <Link className="nav-link" to="/" onClick={handleHomeClick}>
               Inicio
             </Link>
           </li>
